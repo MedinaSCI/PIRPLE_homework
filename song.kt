abbb3aef9359bd18f5fe54ad7c9aeec9bec822a0
MedinaSCI <75686168+MedinaSCI@users.noreply.github.com>
2021-01-05 00:13:42 +0100

    Add files via upload

---
+++ song.kt
@@ -0,0 +1,40 @@
+/* 
+I am Medina. This code is homework in witch I will
+demonstrate use of the different tipe of variables in Kotlin. */ 
+
+val code_of_song: Int=1234 
+//this is some kind of special code for this song
+println("Code of the song: $code_of_song")
+
+val name_of_song: String="Sweet Child O'mine"
+println("Name of the song: $name_of_song")
+
+val name_of_music_group: String="Guns N'Roses"
+println("Name of the music group: $name_of_music_group")
+
+val artist_name: String="William Axl Rose"
+println("Artist name: $artist_name")
+
+var genere: String="Hard Rock"
+println("Genere: $genere")
+
+var album_name: String="Appetite for Destruction"
+println("Album name: $album_name")
+
+var year_of_publishing: Int=1987
+println("Year of the publishing: $year_of_publishing")
+
+var duration: Float=5.02f
+println("Duration: $duration")
+
+var first_letter: Char='S'
+println("First letter: $first_letter")
+
+var important_moment: Double=2.12345
+//this is exact moment in song witch I like
+println("Important moment: $important_moment")
+
+val before_2000: Boolean=true
+println("This song is published before 2000's: $before_2000")
+
+
